8ce5216743848796d6307ae44aece474f4e360cb
Christopher Baptiste <chrisbaptiste83@gmail.com>
2019-08-11 02:31:23 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -4,7 +4,10 @@ class EmailAddressParser
     @emails =emails 
   end 
   def parse(emails)
-    emails.split("'") 
+    emails.split.collect do |address|
+      address.split(',') 
+    end
+    .flatten.uniq 
   end 
-    
+end 
 end 

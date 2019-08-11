ae4bc659a9d37da32486374c83247b3589784e44
Christopher Baptiste <chrisbaptiste83@gmail.com>
2019-08-11 02:33:13 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -3,7 +3,7 @@ class EmailAddressParser
   def initialize(emails)
     @emails =emails 
   end 
-  def parse(emails)
+  def parse
     	@emails = @emails.gsub(/[\s,]/ ," ").split
 		
 		@emails.uniq

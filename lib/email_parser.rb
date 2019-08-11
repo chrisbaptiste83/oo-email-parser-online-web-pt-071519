dce00a9cbbbc52ca97172cebd6a3abe8b58f6e6f
Christopher Baptiste <chrisbaptiste83@gmail.com>
2019-08-11 02:32:51 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -4,9 +4,8 @@ class EmailAddressParser
     @emails =emails 
   end 
   def parse(emails)
-    emails.split.collect do |address|
-      address.split(',') 
-    end
-    .flatten.uniq 
-  end 
+    	@emails = @emails.gsub(/[\s,]/ ," ").split
+		
+		@emails.uniq
+	end
 end 
